bf6bc8909ae1183d1e76481cb8673ba4ec59e26e
Edward Kim <edward.hk.kim@gmail.com>
2014-05-22 18:55:43 -0400

    Edited skeleton functions to cache matrix and solve for inverse of matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## a set of functions to solve the inverse of a matrix and
+## cache the result.
 
-## Write a short comment describing this function
+## makeCacheMatrix caches a matrix and it's inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmat <- function(inv) m <<- inv
+  getmat <- function() m
+  list(set = set, get = get,
+       setmat = setmat,
+       getmat = getmat)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes as input a matrix and computes its inverse
+## if the inverse has been solved this returns the cached result
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getmat()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setmat(m)
+  m
 }
